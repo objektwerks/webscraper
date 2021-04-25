eaba7de47b8850ed5a0f436b62dd67b622dbdbaa
objektwerks <objektwerks@runbox.com>
2021-04-25 08:56:12 -0400

    added basic url get

--- src/main/scala/objektwerks/Main.scala
+++ src/main/scala/objektwerks/Main.scala
@@ -1,7 +1,12 @@
 package objektwerks
 
+import org.jsoup._
+
 object Main {
-  def main(args: Array[String]) {
-    println("web scraping...")
+  def main(args: Array[String]): Unit = {
+    val url = "https://github.com/objektwerks"
+    val doc = Jsoup.connect(url).get()
+    println(s"url: $url")
+    println(s"title: ${doc.title()}")
   }
 }
