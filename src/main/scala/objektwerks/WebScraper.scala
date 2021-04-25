db0a6ed7ff121ee6498b3ef4da9ad94be94a55c3
objektwerks <objektwerks@runbox.com>
2021-04-25 08:58:50 -0400

    renamed main to web scraper

--- src/main/scala/objektwerks/WebScraper.scala
+++ src/main/scala/objektwerks/WebScraper.scala
@@ -2,11 +2,11 @@ package objektwerks
 
 import org.jsoup._
 
-object Main {
+object WebScraper {
   def main(args: Array[String]): Unit = {
     val url = "https://github.com/objektwerks"
     val doc = Jsoup.connect(url).get()
-    println(s"url: $url")
-    println(s"title: ${doc.title()}")
+    println(s"*** url: $url")
+    println(s"*** title: ${doc.title()}")
   }
 }
