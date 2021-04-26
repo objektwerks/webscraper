a40fb04968ea365d780a7b7366fcd43744e927d3
objektwerks <objektwerks@runbox.com>
2021-04-26 09:08:18 -0400

    added org data

--- src/main/scala/objektwerks/WebScraper.scala
+++ src/main/scala/objektwerks/WebScraper.scala
@@ -12,8 +12,14 @@ object WebScraper {
     val doc = Jsoup.connect(url).get()
     println(s"*** title: ${doc.title()}")
 
+    val organization = doc.select(".vcard-details li div").asScala.last.text
+    println(s"*** organization: $organization")
+
     val profile = doc.select("div.p-note.user-profile-bio.mb-3.js-user-profile-bio.f4").asScala.last.text
     println(s"*** profile: $profile")
+
+    val email = doc.select(".vcard-details li div").asScala.last.text
+    println(s"*** email: $email")
 
     val contributions = doc.select("h2.f4.text-normal.mb-2").asScala.last.text()
     println(s"*** contributions: $contributions")
