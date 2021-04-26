1438e382c9a2311196accce61bc9bf473071df0e
objektwerks <objektwerks@runbox.com>
2021-04-26 16:43:13 -0400

    simplified github scraper

--- src/main/scala/objektwerks/GithubScraper.scala
+++ src/main/scala/objektwerks/GithubScraper.scala
@@ -4,38 +4,19 @@ import org.jsoup._
 
 import scala.jdk.CollectionConverters._
 
-case class GithubScraper(url: String,
-                         title: String,
-                         organization: String,
-                         location: String,
-                         experience: String,
-                         resume: String,
-                         contributions: String) {
-  override def toString: String = {
-    val builder = new StringBuilder(8)
+object GithubScraper {
+  def scrape(url: String): String = {
+    val doc = Jsoup.connect(url).get()
+    val builder = new StringBuilder(9)
     builder ++= s"\ngithub scraper\n"
     builder ++= s"--------------\n"
     builder ++= s"- url: $url\n"
-    builder ++= s"- title: $title\n"
-    builder ++= s"- organization: $organization\n"
-    builder ++= s"- experience: $experience\n"
-    builder ++= s"- resume: $resume\n"
-    builder ++= s"- contributions: $contributions\n"
+    builder ++= s"- title: ${doc.title}\n"
+    builder ++= s"- organization: ${doc.select(".vcard-details li div").asScala.last.text}\n"
+    builder ++= s"- location: ${doc.select(".vcard-detail .p-label").asScala.last.text}\n"
+    builder ++= s"- experience: ${doc.select("div.p-note.user-profile-bio.mb-3.js-user-profile-bio.f4").asScala.last.text}\n"
+    builder ++= s"- resume: ${doc.select("li.vcard-detail.pt-1 a.Link--primary").asScala.last.text}\n"
+    builder ++= s"- contributions: ${doc.select("h2.f4.text-normal.mb-2").asScala.last.text}\n"
     builder.result()
-  }
-}
-
-object GithubScraper {
-  def scrape(url: String): GithubScraper = {
-    val doc = Jsoup.connect(url).get()
-    GithubScraper(
-      url = url,
-      title = doc.title(),
-      organization = doc.select(".vcard-details li div").asScala.last.text,
-      location = doc.select(".vcard-detail .p-label").asScala.last.text,
-      experience = doc.select("div.p-note.user-profile-bio.mb-3.js-user-profile-bio.f4").asScala.last.text,
-      resume = doc.select("li.vcard-detail.pt-1 a.Link--primary").asScala.last.text,
-      contributions = doc.select("h2.f4.text-normal.mb-2").asScala.last.text()
-    )
   }
 }
