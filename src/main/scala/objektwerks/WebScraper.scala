b531158aba8d7806dda8cb5b74a46b779ae04c2d
objektwerks <objektwerks@runbox.com>
2021-04-26 08:31:49 -0400

    added profile

--- src/main/scala/objektwerks/WebScraper.scala
+++ src/main/scala/objektwerks/WebScraper.scala
@@ -7,9 +7,13 @@ import scala.jdk.CollectionConverters._
 object WebScraper {
   def main(args: Array[String]): Unit = {
     val url = "https://github.com/objektwerks"
-    val doc = Jsoup.connect(url).get()
     println(s"*** url: $url")
+
+    val doc = Jsoup.connect(url).get()
     println(s"*** title: ${doc.title()}")
+
+    val profile = doc.select("div.p-note.user-profile-bio.mb-3.js-user-profile-bio.f4").asScala.last.text
+    println(s"*** profile: $profile")
 
     val contributions = doc.select("h2.f4.text-normal.mb-2").asScala.last.text()
     println(s"*** contributions: $contributions")
