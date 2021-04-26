daaba8da0a2ac8ebcea6a2f9c4a656ac97bf4660
objektwerks <objektwerks@runbox.com>
2021-04-26 08:24:25 -0400

    added doc.select

--- src/main/scala/objektwerks/WebScraper.scala
+++ src/main/scala/objektwerks/WebScraper.scala
@@ -2,12 +2,16 @@ package objektwerks
 
 import org.jsoup._
 
+import scala.jdk.CollectionConverters._
+
 object WebScraper {
   def main(args: Array[String]): Unit = {
     val url = "https://github.com/objektwerks"
     val doc = Jsoup.connect(url).get()
     println(s"*** url: $url")
     println(s"*** title: ${doc.title()}")
-    println(s"*** body: ${doc.body()}")
+
+    val contributions = doc.select("h2.f4.text-normal.mb-2").asScala.last.text()
+    println(s"*** contributions: $contributions")
   }
 }
